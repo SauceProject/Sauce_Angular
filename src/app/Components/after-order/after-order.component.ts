f18a5c8f979e17407dd6a133db5489f0b0ac174d
BasmaEsmail <basmaabas755@gmail.com>
2022-07-31 16:12:01 +0200

    bg

--- src/app/Components/after-order/after-order.component.ts
+++ src/app/Components/after-order/after-order.component.ts
@@ -14,6 +14,7 @@ export class AfterOrderComponent implements OnInit {
 
   constructor(private http:HttpClient,private acc:AccountServices) { }
   orderID:number=0;
+  
   ngOnInit(): void {
     //Loading.next(true);
     this.getLastOrder();
