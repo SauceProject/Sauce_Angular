cc50737533cd72bc9b59510106f8157fb724a830
a1b1f <ahmedrafie3@gmail.com>
2022-07-31 13:48:08 +0200

    gard

--- src/app/Components/after-order/after-order.component.ts
+++ src/app/Components/after-order/after-order.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { AccountServices } from 'src/app/Services/Account';
 
 @Component({
   selector: 'app-after-order',
@@ -7,9 +9,10 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AfterOrderComponent implements OnInit {
 
-  constructor() { }
+  constructor(private acc:AccountServices,private router:Router) { }
 
   ngOnInit(): void {
+
   }
 
 }
